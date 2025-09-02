3885aea0daddb034ddafabce9877a4cf655970fb
Emily Kanarek <emilyakanarek@gmail.com>
2025-09-02 10:22:10 -0400

    Write tests for Calculator methods

--- spec/student/calculator_spec.rb
+++ spec/student/calculator_spec.rb
@@ -1,5 +1,19 @@
 # frozen_string_literal: true
 
-describe Calculator do
-  # ...student specs...
+describe Calculator do 
+  it "adds two numbers" do 
+    expect(Calculator.new.add(2, 5)).to eq(7)
+  end
+
+  it "subtracts one number from another number" do 
+    expect(Calculator.new.subtract(5, 2)).to eq(3)
+  end
+
+  it "multiplies two numbers" do 
+    expect(Calculator.new.multiply(5, 2)).to eq(10)
+  end
+
+  it "divides one number by another number" do 
+    expect(Calculator.new.divide(10, 2)).to eq(5)
+  end
 end
