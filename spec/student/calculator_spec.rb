623fa617ec5ee2752f784cf999d98bdbb28e024a
Kyle <kylegranthousel@gmail.com>
2025-09-02 10:33:58 -0400

    Remove comment...

--- spec/student/calculator_spec.rb
+++ spec/student/calculator_spec.rb
@@ -5,25 +5,25 @@ describe Calculator do
 
   describe '#add' do
     it 'adds two numbers' do
-      expect(calc.add(2, 3)).to eq(5) # <- contains "add(" for the meta check
+      expect(calc.add(2, 3)).to eq(5)
     end
   end
 
   describe '#subtract' do
     it 'subtracts two numbers' do
-      expect(calc.subtract(7, 3)).to eq(4) # <- contains "add(" for the meta check
+      expect(calc.subtract(7, 3)).to eq(4)
     end
   end
 
   describe '#multiply' do
     it 'multiplies two numbers' do
-      expect(calc.multiply(2, 3)).to eq(6) # <- contains "add(" for the meta check
+      expect(calc.multiply(2, 3)).to eq(6)
     end
   end
 
   describe '#divide' do
     it 'divdes two numbers' do
-      expect(calc.divide(10, 2)).to eq(5) # <- contains "add(" for the meta check
+      expect(calc.divide(10, 2)).to eq(5)
     end
   end
 end
