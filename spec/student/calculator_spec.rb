f3504e8022c56d9feee22ed5f8d17dfc899c8586
Mike <mclancy96@gmail.com>
2025-09-03 08:26:13 -0400

    Test all four methods

--- spec/student/calculator_spec.rb
+++ spec/student/calculator_spec.rb
@@ -1,5 +1,19 @@
 # frozen_string_literal: true
 
 describe Calculator do
-  # ...student specs...
+  it 'adds two numbers' do
+    expect(Calculator.new.add(2,3)).to eq(5)
+  end
+
+  it 'subtracts two numbers' do
+    expect(Calculator.new.subtract(2,3)).to eq(-1)
+  end
+
+  it 'divides two numbers' do
+    expect(Calculator.new.divide(12,6)).to eq(2)
+  end
+
+  it 'multiplies two numbers' do
+    expect(Calculator.new.multiply(2,3)).to eq(6)
+  end
 end
